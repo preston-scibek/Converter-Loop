41e43519c649009739b73ec0c22e947ff8f9f5a5
Preston Scibek <preston@bytelion.com>
2021-09-16 10:00:27 -0400

    Adding converter loop to git

---
+++ README.md
@@ -0,0 +1,27 @@
+# Olive Helps: Converter-Loop
+This loop converts imperial to metric and vice versa when you copy a string.
+
+## Requirements
+
+You will need to install [Olive Helps](https://oliveai.com/olive-helps/).
+
+Install [Node.js](https://nodejs.org/), clone this repo, and install the project dependencies with:
+```shell
+npm install
+```
+
+Then compile and package the project with:
+```shell
+npm run build
+```
+
+## Other Commands
+
+```shell
+# ESLint and Prettier
+npm run lint          # Check for issues
+npm run lint:fix      # Fix all auto-fixable issues
+
+# Watch
+npm run watch          # Watch for changes and rebuild as needed
+```
---
+++ src/index.js
@@ -0,0 +1,103 @@
+const { clipboard, whisper } = require('@oliveai/ldk');
+
+const writeWhisper = (labelV, body) => {
+  whisper.create({
+    label: labelV,
+    onClose: () => { console.log('Closed Whisper') },
+    components: [
+        {
+            body: body,
+            type: whisper.WhisperComponentType.Markdown,
+        },
+    ]
+  });
+};
+
+
+function convertLbsToKG(lb){
+  return lb/2.2046;
+}
+
+function convertKgToLbs(kg){
+  return kg*2.2046;
+}
+
+function convertFtToM(ft){
+  return ft/3.2808;
+}
+
+function convertMToFt(m){
+  return m*3.2808;
+}
+
+function convertFToC(f){
+  return (f-32)/1.8;
+}
+
+function convertCToF(c){
+  return (c*1.8)+32;
+}
+
+function convertMphToKph(mph){
+  return mph*1.609344;
+}
+
+function convertKphToMph(kph){
+  return kph/1.609344;
+}
+
+async function converterCallback(incomingText) {
+  let value = parseFloat(incomingText.split(/[^\d+]/)[0]);
+  let unit = incomingText.split(/\d+/)[1];
+  let resVal;
+  let resUnit;
+  console.log(unit);
+  if (unit === null || unit === undefined){
+    return;
+  }
+  switch(unit.toLowerCase()){
+    case "kg":
+      resVal = convertKgToLbs(value);
+      resUnit = 'lb';
+      break;
+    case "lb":
+      resVal = convertLbsToKG(value);
+      resUnit = 'kg';
+      break;
+    case "ft":
+      resVal = convertFtToM(value);
+      resUnit = 'm';
+      break;
+    case "m":
+      resVal = convertMToFt(value);
+      resUnit = 'ft';
+      break;
+    case "mph":
+      resVal = convertMphToKph(value);
+      resUnit = 'kph';
+      break;
+    case "kph":
+      resVal = convertKphToMph(value);
+      resUnit = 'mph';
+      break;
+    case "f":
+      resVal = convertFToC(value);
+      resUnit = 'c';
+      break;
+    case "c":
+      resVal = convertCToF(value);
+      resUnit = 'f';
+      break;
+    default:
+      return;
+  }
+
+  writeWhisper("Conversion:", `${value}${unit} converts to ${resVal}${resUnit}`)
+}
+
+async function converterWhisper() {
+  writeWhisper("Startup:", "Starting Clipboard listener")
+  clipboard.listen(true, converterCallback);
+}
+
+converterWhisper();
