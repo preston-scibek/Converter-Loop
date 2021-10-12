49ab9ea629c0bcdab3c937e933ed546d3e78da56
Preston Scibek <preston@bytelion.com>
2021-10-12 10:20:36 -0400

    linting

--- src/index.js
+++ src/index.js
@@ -3,88 +3,89 @@ const { clipboard, whisper } = require('@oliveai/ldk');
 const writeWhisper = (labelV, body) => {
   whisper.create({
     label: labelV,
-    onClose: () => { console.log('Closed Whisper') },
+    onClose: () => {
+      console.log(`Closed Whisper`);
+    },
     components: [
-        {
-            body: body,
-            type: whisper.WhisperComponentType.Markdown,
-        },
-    ]
+      {
+        body,
+        type: whisper.WhisperComponentType.Markdown,
+      },
+    ],
   });
 };
 
-
-function convertLbsToKG(lb){
-  return lb/2.2046;
+function convertLbsToKG(lb) {
+  return lb / 2.2046;
 }
 
-function convertKgToLbs(kg){
-  return kg*2.2046;
+function convertKgToLbs(kg) {
+  return kg * 2.2046;
 }
 
-function convertFtToM(ft){
-  return ft/3.2808;
+function convertFtToM(ft) {
+  return ft / 3.2808;
 }
 
-function convertMToFt(m){
-  return m*3.2808;
+function convertMToFt(m) {
+  return m * 3.2808;
 }
 
-function convertFToC(f){
-  return (f-32)/1.8;
+function convertFToC(f) {
+  return (f - 32) / 1.8;
 }
 
-function convertCToF(c){
-  return (c*1.8)+32;
+function convertCToF(c) {
+  return c * 1.8 + 32;
 }
 
-function convertMphToKph(mph){
-  return mph*1.609344;
+function convertMphToKph(mph) {
+  return mph * 1.609344;
 }
 
-function convertKphToMph(kph){
-  return kph/1.609344;
+function convertKphToMph(kph) {
+  return kph / 1.609344;
 }
 
 async function converterCallback(incomingText) {
-  let value = parseFloat(incomingText.split(/[^\d+]/)[0]);
-  let unit = incomingText.split(/\d+/)[1];
+  const value = parseFloat(incomingText.split(/[^\d+]/)[0]);
+  const unit = incomingText.split(/\d+/)[1];
   let resVal;
   let resUnit;
   console.log(unit);
-  if (unit === null || unit === undefined){
+  if (unit === null || unit === undefined) {
     return;
   }
-  switch(unit.toLowerCase()){
-    case "kg":
+  switch (unit.toLowerCase()) {
+    case 'kg':
       resVal = convertKgToLbs(value);
       resUnit = 'lb';
       break;
-    case "lb":
+    case 'lb':
       resVal = convertLbsToKG(value);
       resUnit = 'kg';
       break;
-    case "ft":
+    case 'ft':
       resVal = convertFtToM(value);
       resUnit = 'm';
       break;
-    case "m":
+    case 'm':
       resVal = convertMToFt(value);
       resUnit = 'ft';
       break;
-    case "mph":
+    case 'mph':
       resVal = convertMphToKph(value);
       resUnit = 'kph';
       break;
-    case "kph":
+    case 'kph':
       resVal = convertKphToMph(value);
       resUnit = 'mph';
       break;
-    case "f":
+    case 'f':
       resVal = convertFToC(value);
       resUnit = 'c';
       break;
-    case "c":
+    case 'c':
       resVal = convertCToF(value);
       resUnit = 'f';
       break;
@@ -92,11 +93,11 @@ async function converterCallback(incomingText) {
       return;
   }
 
-  writeWhisper("Converter-Loop:", `${value}${unit} converts to ${resVal}${resUnit}`)
+  writeWhisper('Converter-Loop:', `${value}${unit} converts to ${resVal}${resUnit}`);
 }
 
 async function converterWhisper() {
-  writeWhisper("Converter-Loop:", "Starting Converter-Loop")
+  writeWhisper('Converter-Loop:', 'Starting Converter-Loop');
   clipboard.listen(true, converterCallback);
 }
 
